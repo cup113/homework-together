bc3fd6be25c665c19b4176c175195c22375c829d
cup11 <cup11jason@qq.com>
2024-10-07 18:52:03 +0800

    server-side migration

---
+++ pb_migrations/1728297585_updated_publicItems.js
@@ -0,0 +1,18 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("s4vdwioo98qyih1")
+
+  collection.listRule = "@request.auth.id = author.id || @request.auth.organizations.id ?= organization.id"
+  collection.viewRule = "@request.auth.id = author.id || @request.auth.organizations.id ?= organization.id"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("s4vdwioo98qyih1")
+
+  collection.listRule = "(range != \"private\" && @request.auth.organizations.id ?= organization.id) || @request.auth.id = author.id"
+  collection.viewRule = "(range != \"private\" && @request.auth.organizations.id ?= organization.id) || @request.auth.id = author.id"
+
+  return dao.saveCollection(collection)
+})
