3b445dcd4c75a13bdb86d8bcced65647831d53b7
Jason Li <jasonmli@outlook.com>
2024-12-01 09:55:42 +0800

    feat: add visiting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavBar from '@/components/NavBar.vue';
-import { useNetworkStore } from '@/stores/network';
+import MyFooter from './components/MyFooter.vue';
 
-const networkStore = useNetworkStore();
 </script>
 
 <template>
@@ -15,13 +14,6 @@ const networkStore = useNetworkStore();
     <main class="flex-grow">
       <RouterView></RouterView>
     </main>
-    <footer class="bg-slate-100 text-slate-700 text-center text-sm mt-8 py-4">
-      <div class="flex flex-col md:flex-row gap-2 md:gap-8 justify-center">
-        <div>Homework Together Since 2024.9</div>
-        <div><a href="https://github.com/cup113/homework-together" target="_blank"
-            class="underline hover:bg-slate-200 rounded-md">Open Source: Github</a></div>
-        <div>Build Version: {{ networkStore.buildVersion }}</div>
-      </div>
-    </footer>
+    <MyFooter></MyFooter>
   </div>
 </template>
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+import { useNetworkStore } from '@/stores/network';
+
+const networkStore = useNetworkStore();
+</script>
+
+<template>
+    <footer class="bg-slate-100 text-slate-700 text-center text-sm mt-8 py-4">
+        <div class="flex flex-col md:flex-row gap-2 md:gap-8 justify-center">
+            <div>Homework Together Since 2024.9</div>
+            <div>
+                <a href="https://github.com/cup113/homework-together" target="_blank"
+                    class="hover:bg-slate-200 rounded-md gap-1 flex items-center justify-center">
+                    <Icon icon="mdi:github" class="h-4 w-4"></Icon>Open Source
+                </a>
+            </div>
+            <div>Build Version: {{ networkStore.buildVersion }}</div>
+            <div class="flex justify-center items-center">
+                <img src="https://badges.pufler.dev/visits/cup113/homework-together">
+            </div>
+        </div>
+    </footer>
+</template>
